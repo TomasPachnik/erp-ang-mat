3ccaa1f1545826c51c4bd7fd74d1f5a4eeeebc00
Tomáš Páchnik <tomas.pachnik@sigp.eu>
2019-01-03 11:57:45 +0100

    default route to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TableComponent } from './table/table.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavComponent} from './nav/nav.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatMenuModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+} from '@angular/material';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TableComponent} from './table/table.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HomePageComponent} from './home-page/home-page.component';
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomePageComponent }
+  {path: '', redirectTo: "/home", pathMatch: 'full'},
+  {path: 'home', component: HomePageComponent}
 ];
 
 @NgModule({
@@ -45,4 +58,5 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
