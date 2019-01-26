98b7cee66aba98547ece5972403a058c39470260
Tomáš Páchnik <tomas.pachnik@gmail.com>
2019-01-26 19:33:20 +0100

    chat WIP

--- src/app/chat/chat/chat.component.ts
+++ src/app/chat/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  items = ['aa', 'bb', 'cc'];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
