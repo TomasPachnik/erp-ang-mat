571d254c29bb700a5eaba4954b941d897e9b6c49
Tomáš Páchnik <tomas.pachnik@gmail.com>
2019-02-02 19:28:09 +0100

    invoice translation

--- src/app/invoice/invoice-detail/invoice-detail.component.ts
+++ src/app/invoice/invoice-detail/invoice-detail.component.ts
@@ -2,102 +2,105 @@ import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {RestService} from '../../rest.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {TranslateService} from "@ngx-translate/core";
+import {LangService} from "../../lang.service";
 
 @Component({
-  selector: 'app-invoice-detail',
-  templateUrl: './invoice-detail.component.html',
-  styleUrls: ['./invoice-detail.component.css']
+    selector: 'app-invoice-detail',
+    templateUrl: './invoice-detail.component.html',
+    styleUrls: ['./invoice-detail.component.css']
 })
 export class InvoiceDetailComponent implements OnInit {
 
-  invoiceDetailsForm = this.fb.group({
-    uuid: [null],
-    name: ['', Validators.required],
-    invoiceNumber: ['', Validators.required],
-    supplierVariableSymbol: ['', Validators.required],
-    currency: ['', Validators.required],
-    customer: ['', Validators.required],
-    supplier: ['', Validators.required],
-    dateOfIssue: [new Date(), Validators.required],
-    deliveryDate: ['', Validators.required],
-    dueDate: ['', Validators.required],
-    note: [''],
-  });
+    invoiceDetailsForm = this.fb.group({
+        uuid: [null],
+        name: ['', Validators.required],
+        invoiceNumber: ['', Validators.required],
+        supplierVariableSymbol: ['', Validators.required],
+        currency: ['', Validators.required],
+        customer: ['', Validators.required],
+        supplier: ['', Validators.required],
+        dateOfIssue: [new Date(), Validators.required],
+        deliveryDate: ['', Validators.required],
+        dueDate: ['', Validators.required],
+        note: [''],
+    });
 
-  assetForm: FormGroup;
+    assetForm: FormGroup;
 
-  customers: any = [];
-  suppliers: any = [];
+    customers: any = [];
+    suppliers: any = [];
 
-  constructor(private fb: FormBuilder, private rest: RestService, private router: Router, private route: ActivatedRoute) {
-  }
-
-  ngOnInit() {
-    this.getCustomers();
-    this.getSuppliers();
-    if (this.route.snapshot.params['uuid'] !== 'new-invoice') {
-      this.getInvoiceDetail();
+    constructor(private fb: FormBuilder, private rest: RestService, private router: Router, private route: ActivatedRoute, private translate: TranslateService) {
+        translate.setDefaultLang(LangService.getLanguage());
     }
-    this.assetForm = this.fb.group({
-      items: this.fb.array([])
-    });
-  }
 
-  getCustomers() {
-    this.rest.getCustomers().subscribe((invoices: {}) => {
-      this.customers = invoices;
-    });
-  }
-
-  getSuppliers() {
-    this.rest.getSuppliers().subscribe((data: {}) => {
-      this.suppliers = data;
-    });
-  }
-
-  getInvoiceDetail() {
-    this.rest.getInvoice(this.route.snapshot.params['uuid']).subscribe((data: {}) => {
-      this.mapFromInvoice(data);
-    });
-  }
-
-  get assetsFormData() {
-    return <FormArray>this.assetForm.get('items');
-  }
-
-  private mapFromInvoice(data) {
-    this.invoiceDetailsForm.controls['uuid'].setValue(data.uuid);
-    this.invoiceDetailsForm.controls['name'].setValue(data.name);
-    this.invoiceDetailsForm.controls['invoiceNumber'].setValue(data.invoiceNumber);
-    this.invoiceDetailsForm.controls['supplierVariableSymbol'].setValue(data.supplierVariableSymbol);
-    this.invoiceDetailsForm.controls['currency'].setValue(data.currency);
-    this.invoiceDetailsForm.controls['customer'].setValue(data.customer.uuid);
-    this.invoiceDetailsForm.controls['supplier'].setValue(data.supplier.uuid);
-    this.invoiceDetailsForm.controls['dateOfIssue'].setValue(new Date(data.dateOfIssue));
-    this.invoiceDetailsForm.controls['deliveryDate'].setValue(new Date(data.deliveryDate));
-    this.invoiceDetailsForm.controls['dueDate'].setValue(new Date(data.dueDate));
-    this.invoiceDetailsForm.controls['note'].setValue(data.note);
-    for (const item of data.assets) {
-      // @ts-ignore
-      this.assetForm.get('items').push(this.buildItem(item.uuid, item.name, item.count, item.unit, item.unitPrice));
+    ngOnInit() {
+        this.getCustomers();
+        this.getSuppliers();
+        if (this.route.snapshot.params['uuid'] !== 'new-invoice') {
+            this.getInvoiceDetail();
+        }
+        this.assetForm = this.fb.group({
+            items: this.fb.array([])
+        });
     }
-  }
 
-  buildItem(uuid: string, name: string, count: number, unit: string, unitPrice: number) {
-    return new FormGroup({
-      uuid: new FormControl(uuid, Validators.required),
-      name: new FormControl(name, Validators.required),
-      count: new FormControl(count, Validators.required),
-      unit: new FormControl(unit, Validators.required),
-      unitPrice: new FormControl(unitPrice, Validators.required),
-    });
-  }
+    getCustomers() {
+        this.rest.getCustomers().subscribe((invoices: {}) => {
+            this.customers = invoices;
+        });
+    }
 
-  onSubmitInvoiceDetailsForm() {
-    const invoice = this.invoiceDetailsForm.getRawValue();
-    invoice.assets = this.assetForm.getRawValue().items;
-    this.rest.updateInvoice(invoice).subscribe(() => {
-      this.router.navigate(['invoices']);
-    });
-  }
+    getSuppliers() {
+        this.rest.getSuppliers().subscribe((data: {}) => {
+            this.suppliers = data;
+        });
+    }
+
+    getInvoiceDetail() {
+        this.rest.getInvoice(this.route.snapshot.params['uuid']).subscribe((data: {}) => {
+            this.mapFromInvoice(data);
+        });
+    }
+
+    get assetsFormData() {
+        return <FormArray>this.assetForm.get('items');
+    }
+
+    private mapFromInvoice(data) {
+        this.invoiceDetailsForm.controls['uuid'].setValue(data.uuid);
+        this.invoiceDetailsForm.controls['name'].setValue(data.name);
+        this.invoiceDetailsForm.controls['invoiceNumber'].setValue(data.invoiceNumber);
+        this.invoiceDetailsForm.controls['supplierVariableSymbol'].setValue(data.supplierVariableSymbol);
+        this.invoiceDetailsForm.controls['currency'].setValue(data.currency);
+        this.invoiceDetailsForm.controls['customer'].setValue(data.customer.uuid);
+        this.invoiceDetailsForm.controls['supplier'].setValue(data.supplier.uuid);
+        this.invoiceDetailsForm.controls['dateOfIssue'].setValue(new Date(data.dateOfIssue));
+        this.invoiceDetailsForm.controls['deliveryDate'].setValue(new Date(data.deliveryDate));
+        this.invoiceDetailsForm.controls['dueDate'].setValue(new Date(data.dueDate));
+        this.invoiceDetailsForm.controls['note'].setValue(data.note);
+        for (const item of data.assets) {
+            // @ts-ignore
+            this.assetForm.get('items').push(this.buildItem(item.uuid, item.name, item.count, item.unit, item.unitPrice));
+        }
+    }
+
+    buildItem(uuid: string, name: string, count: number, unit: string, unitPrice: number) {
+        return new FormGroup({
+            uuid: new FormControl(uuid, Validators.required),
+            name: new FormControl(name, Validators.required),
+            count: new FormControl(count, Validators.required),
+            unit: new FormControl(unit, Validators.required),
+            unitPrice: new FormControl(unitPrice, Validators.required),
+        });
+    }
+
+    onSubmitInvoiceDetailsForm() {
+        const invoice = this.invoiceDetailsForm.getRawValue();
+        invoice.assets = this.assetForm.getRawValue().items;
+        this.rest.updateInvoice(invoice).subscribe(() => {
+            this.router.navigate(['invoices']);
+        });
+    }
 }
--- src/app/invoice/invoices/invoices.component.ts
+++ src/app/invoice/invoices/invoices.component.ts
@@ -3,95 +3,102 @@ import {MatPaginator, MatTableDataSource, PageEvent} from '@angular/material';
 import {Router} from '@angular/router';
 import {RestService} from '../../rest.service';
 import * as FileSaver from 'file-saver';
+import {TranslateService} from "@ngx-translate/core";
+import {LangService} from "../../lang.service";
 
 export class Invoice {
-  uuid: string;
-  name: string;
-  invoiceNumber: string;
-  supplier: string;
-  customer: string;
-  dateOfIssue: Date;
-  deliveryDate: Date;
-  dueDate: Date;
-  price: string;
-  issuer: string;
+    uuid: string;
+    name: string;
+    invoiceNumber: string;
+    supplier: string;
+    customer: string;
+    dateOfIssue: Date;
+    deliveryDate: Date;
+    dueDate: Date;
+    price: string;
+    issuer: string;
 }
 
 const ELEMENT_DATA: Invoice[] = [];
 
 @Component({
-  selector: 'app-invoices',
-  templateUrl: './invoices.component.html',
-  styleUrls: ['./invoices.component.css']
+    selector: 'app-invoices',
+    templateUrl: './invoices.component.html',
+    styleUrls: ['./invoices.component.css']
 })
 export class InvoicesComponent implements OnInit {
 
-  displayedColumns: string[] = ['name', 'supplier', 'customer', 'dateOfIssue', 'deliveryDate', 'dueDate', 'price', 'actions'];
-  dataSource = new MatTableDataSource<Invoice>(ELEMENT_DATA);
-  defaultPageSize = 10;
+    displayedColumns: string[] = ['name', 'supplier', 'customer', 'dateOfIssue', 'deliveryDate', 'dueDate', 'price', 'actions'];
+    dataSource = new MatTableDataSource<Invoice>(ELEMENT_DATA);
+    defaultPageSize = 10;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    sure: string;
 
-  constructor(private router: Router, private rest: RestService) {
-  }
-
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-    this.getInvoices(this.paginator.pageIndex, this.defaultPageSize);
-  }
-
-
-  getTotalCost() {
-    let result = 0;
-    for (const entry of this.dataSource.data) {
-      // @ts-ignore
-      result += entry.total;
+    constructor(private router: Router, private rest: RestService, private translate: TranslateService) {
+        translate.setDefaultLang(LangService.getLanguage());
+        translate.get('customers.sure').subscribe((translated: string) => {
+            this.sure = translated;
+        });
     }
-    return result;
-  }
 
-  getCurrency() {
-    if (this.dataSource.data.length > 0) {
-      // @ts-ignore
-      return this.dataSource.data[0].currency;
+    ngOnInit() {
+        this.dataSource.paginator = this.paginator;
+        this.getInvoices(this.paginator.pageIndex, this.defaultPageSize);
     }
-    return '';
-  }
 
-  onPdf(invoice) {
-    this.rest.generateInvoice(invoice.uuid).subscribe((file: Blob) => {
-      const filename = invoice.name + '.pdf';
-      FileSaver.saveAs(file, filename);
-    });
-  }
 
-  onEdit(invoice) {
-    this.router.navigate(['/invoices/' + invoice.uuid]);
-  }
-
-  newEntry() {
-    this.router.navigate(['/invoices/new-invoice']);
-  }
-
-  onDelete(invoice) {
-    if (confirm('Ste si istý?')) {
-      this.rest.removeInvoice(invoice.uuid).subscribe((data: {}) => {
-        this.getInvoices(this.paginator.pageIndex, this.paginator.pageSize);
-      });
+    getTotalCost() {
+        let result = 0;
+        for (const entry of this.dataSource.data) {
+            // @ts-ignore
+            result += entry.total;
+        }
+        return result;
     }
-  }
 
-  handlePage(event: PageEvent) {
-    this.getInvoices(event.pageIndex, event.pageSize);
-  }
+    getCurrency() {
+        if (this.dataSource.data.length > 0) {
+            // @ts-ignore
+            return this.dataSource.data[0].currency;
+        }
+        return '';
+    }
 
-  getInvoices(pageIndex, pageSize) {
-    this.rest.getInvoicesWithPagination(pageIndex, pageSize).subscribe(data => {
-      this.dataSource = new MatTableDataSource<Invoice>(data.content);
-      this.paginator.pageIndex = data.pageable.pageIndex;
-      this.paginator.pageSize = data.pageable.pageSize;
-      this.paginator.length = data.total;
-    });
-  }
+    onPdf(invoice) {
+        this.rest.generateInvoice(invoice.uuid).subscribe((file: Blob) => {
+            const filename = invoice.name + '.pdf';
+            FileSaver.saveAs(file, filename);
+        });
+    }
+
+    onEdit(invoice) {
+        this.router.navigate(['/invoices/' + invoice.uuid]);
+    }
+
+    newEntry() {
+        this.router.navigate(['/invoices/new-invoice']);
+    }
+
+    onDelete(invoice) {
+        if (confirm(this.sure)) {
+            this.rest.removeInvoice(invoice.uuid).subscribe((data: {}) => {
+                this.getInvoices(this.paginator.pageIndex, this.paginator.pageSize);
+            });
+        }
+    }
+
+    handlePage(event: PageEvent) {
+        this.getInvoices(event.pageIndex, event.pageSize);
+    }
+
+    getInvoices(pageIndex, pageSize) {
+        this.rest.getInvoicesWithPagination(pageIndex, pageSize).subscribe(data => {
+            this.dataSource = new MatTableDataSource<Invoice>(data.content);
+            this.paginator.pageIndex = data.pageable.pageIndex;
+            this.paginator.pageSize = data.pageable.pageSize;
+            this.paginator.length = data.total;
+        });
+    }
 
 }
