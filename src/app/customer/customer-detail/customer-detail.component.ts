02b40a6a876ce3149c560aed909c87e898e448ed
Tomáš Páchnik <tomas.pachnik@gmail.com>
2019-02-02 19:04:14 +0100

    customers translation

--- src/app/customer/customer-detail/customer-detail.component.ts
+++ src/app/customer/customer-detail/customer-detail.component.ts
@@ -3,83 +3,86 @@ import {FormBuilder, Validators} from '@angular/forms';
 import {RestService} from '../../rest.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Customer} from '../customers/customers.component';
+import {LangService} from "../../lang.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-customer-detail',
-  templateUrl: './customer-detail.component.html',
-  styleUrls: ['./customer-detail.component.css']
+    selector: 'app-customer-detail',
+    templateUrl: './customer-detail.component.html',
+    styleUrls: ['./customer-detail.component.css']
 })
 export class CustomerDetailComponent implements OnInit {
 
-  customerDetailsForm = this.fb.group({
-    uuid: [null],
-    name: ['', Validators.required],
-    companyIdentificationNumber: ['', Validators.required],
-    taxIdentificationNumber: ['', Validators.required],
-    addressUuid: [null],
-    street: ['', Validators.required],
-    houseNumber: ['', Validators.required],
-    postalCode: ['', Validators.required],
-    town: ['', Validators.required],
-    country: ['', Validators.required],
-    bankName: ['', Validators.required],
-    iban: ['', Validators.required],
-    bankAccountUuid: [null],
-  });
+    customerDetailsForm = this.fb.group({
+        uuid: [null],
+        name: ['', Validators.required],
+        companyIdentificationNumber: ['', Validators.required],
+        taxIdentificationNumber: ['', Validators.required],
+        addressUuid: [null],
+        street: ['', Validators.required],
+        houseNumber: ['', Validators.required],
+        postalCode: ['', Validators.required],
+        town: ['', Validators.required],
+        country: ['', Validators.required],
+        bankName: ['', Validators.required],
+        iban: ['', Validators.required],
+        bankAccountUuid: [null],
+    });
 
-  constructor(private fb: FormBuilder, private rest: RestService, private router: Router, private route: ActivatedRoute) {
-  }
-
-  ngOnInit() {
-    if (this.route.snapshot.params['uuid'] !== 'new-customer') {
-      this.getCustomerDetail();
+    constructor(private fb: FormBuilder, private rest: RestService, private router: Router, private route: ActivatedRoute, private translate: TranslateService) {
+        translate.setDefaultLang(LangService.getLanguage());
     }
-  }
 
-  getCustomerDetail() {
-    this.rest.getCustomer(this.route.snapshot.params['uuid']).subscribe((data: {}) => {
-      this.mapFromCustomer(data);
-    });
-  }
+    ngOnInit() {
+        if (this.route.snapshot.params['uuid'] !== 'new-customer') {
+            this.getCustomerDetail();
+        }
+    }
 
-  onSubmitCustomerDetailsForm(data) {
-    this.rest.updateCustomer(this.mapToCustomer(this.customerDetailsForm.getRawValue())).subscribe(() => {
-      this.router.navigate(['customers']);
-    });
-  }
+    getCustomerDetail() {
+        this.rest.getCustomer(this.route.snapshot.params['uuid']).subscribe((data: {}) => {
+            this.mapFromCustomer(data);
+        });
+    }
 
-  mapFromCustomer(data) {
-    this.customerDetailsForm.controls['uuid'].setValue(data.uuid);
-    this.customerDetailsForm.controls['name'].setValue(data.name);
-    this.customerDetailsForm.controls['companyIdentificationNumber'].setValue(data.companyIdentificationNumber);
-    this.customerDetailsForm.controls['taxIdentificationNumber'].setValue(data.taxIdentificationNumber);
-    this.customerDetailsForm.controls['addressUuid'].setValue(data.address.uuid);
-    this.customerDetailsForm.controls['street'].setValue(data.address.street);
-    this.customerDetailsForm.controls['houseNumber'].setValue(data.address.houseNumber);
-    this.customerDetailsForm.controls['postalCode'].setValue(data.address.postalCode);
-    this.customerDetailsForm.controls['town'].setValue(data.address.town);
-    this.customerDetailsForm.controls['country'].setValue(data.address.country);
-    this.customerDetailsForm.controls['bankAccountUuid'].setValue(data.bankAccount.uuid);
-    this.customerDetailsForm.controls['bankName'].setValue(data.bankAccount.bankName);
-    this.customerDetailsForm.controls['iban'].setValue(data.bankAccount.iban);
-  }
+    onSubmitCustomerDetailsForm(data) {
+        this.rest.updateCustomer(this.mapToCustomer(this.customerDetailsForm.getRawValue())).subscribe(() => {
+            this.router.navigate(['customers']);
+        });
+    }
 
-  mapToCustomer(data) {
-    return new Customer(
-      data.uuid,
-      data.name,
-      data.companyIdentificationNumber,
-      data.taxIdentificationNumber,
-      data.addressUuid,
-      data.street,
-      data.houseNumber,
-      data.postalCode,
-      data.town,
-      data.country,
-      data.bankAccountUuid,
-      data.bankName,
-      data.iban
-    );
-  }
+    mapFromCustomer(data) {
+        this.customerDetailsForm.controls['uuid'].setValue(data.uuid);
+        this.customerDetailsForm.controls['name'].setValue(data.name);
+        this.customerDetailsForm.controls['companyIdentificationNumber'].setValue(data.companyIdentificationNumber);
+        this.customerDetailsForm.controls['taxIdentificationNumber'].setValue(data.taxIdentificationNumber);
+        this.customerDetailsForm.controls['addressUuid'].setValue(data.address.uuid);
+        this.customerDetailsForm.controls['street'].setValue(data.address.street);
+        this.customerDetailsForm.controls['houseNumber'].setValue(data.address.houseNumber);
+        this.customerDetailsForm.controls['postalCode'].setValue(data.address.postalCode);
+        this.customerDetailsForm.controls['town'].setValue(data.address.town);
+        this.customerDetailsForm.controls['country'].setValue(data.address.country);
+        this.customerDetailsForm.controls['bankAccountUuid'].setValue(data.bankAccount.uuid);
+        this.customerDetailsForm.controls['bankName'].setValue(data.bankAccount.bankName);
+        this.customerDetailsForm.controls['iban'].setValue(data.bankAccount.iban);
+    }
+
+    mapToCustomer(data) {
+        return new Customer(
+            data.uuid,
+            data.name,
+            data.companyIdentificationNumber,
+            data.taxIdentificationNumber,
+            data.addressUuid,
+            data.street,
+            data.houseNumber,
+            data.postalCode,
+            data.town,
+            data.country,
+            data.bankAccountUuid,
+            data.bankName,
+            data.iban
+        );
+    }
 
 }
--- src/app/customer/customers/customers.component.ts
+++ src/app/customer/customers/customers.component.ts
@@ -2,107 +2,114 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
 import {Router} from '@angular/router';
 import {RestService} from '../../rest.service';
+import {TranslateService} from "@ngx-translate/core";
+import {LangService} from "../../lang.service";
 
 export class Address {
-  uuid: string;
-  street: string;
-  houseNumber: string;
-  postalCode: string;
-  town: string;
-  country: string;
+    uuid: string;
+    street: string;
+    houseNumber: string;
+    postalCode: string;
+    town: string;
+    country: string;
 
-  constructor(uuid: string, street: string, houseNumber: string, postalCode: string, town: string, country: string) {
-    this.uuid = uuid;
-    this.street = street;
-    this.houseNumber = houseNumber;
-    this.postalCode = postalCode;
-    this.town = town;
-    this.country = country;
-  }
+    constructor(uuid: string, street: string, houseNumber: string, postalCode: string, town: string, country: string) {
+        this.uuid = uuid;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.town = town;
+        this.country = country;
+    }
 }
 
 export class BankAccount {
-  uuid: string;
-  bankName: string;
-  iban: string;
+    uuid: string;
+    bankName: string;
+    iban: string;
 
-  constructor(uuid: string, bankName: string, iban: string) {
-    this.uuid = uuid;
-    this.bankName = bankName;
-    this.iban = iban;
-  }
+    constructor(uuid: string, bankName: string, iban: string) {
+        this.uuid = uuid;
+        this.bankName = bankName;
+        this.iban = iban;
+    }
 }
 
 export class Customer {
-  uuid: string;
-  name: string;
-  companyIdentificationNumber: string;
-  taxIdentificationNumber: string;
-  address: Address;
-  bankAccount: BankAccount;
+    uuid: string;
+    name: string;
+    companyIdentificationNumber: string;
+    taxIdentificationNumber: string;
+    address: Address;
+    bankAccount: BankAccount;
 
-  constructor(uuid: string, name: string, companyIdentificationNumber: string, taxIdentificationNumber: string,
-              addressUuid: string, street: string, houseNumber: string, postalCode: string, town: string, country: string,
-              bankAccountUuid: string, bankName: string, iban: string) {
-    this.uuid = uuid;
-    this.name = name;
-    this.companyIdentificationNumber = companyIdentificationNumber;
-    this.taxIdentificationNumber = taxIdentificationNumber;
-    this.address = new Address(addressUuid, street, houseNumber, postalCode, town, country);
-    this.bankAccount = new BankAccount(bankAccountUuid, bankName, iban);
-  }
+    constructor(uuid: string, name: string, companyIdentificationNumber: string, taxIdentificationNumber: string,
+                addressUuid: string, street: string, houseNumber: string, postalCode: string, town: string, country: string,
+                bankAccountUuid: string, bankName: string, iban: string) {
+        this.uuid = uuid;
+        this.name = name;
+        this.companyIdentificationNumber = companyIdentificationNumber;
+        this.taxIdentificationNumber = taxIdentificationNumber;
+        this.address = new Address(addressUuid, street, houseNumber, postalCode, town, country);
+        this.bankAccount = new BankAccount(bankAccountUuid, bankName, iban);
+    }
 }
 
 const ELEMENT_DATA: Customer[] = [];
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+    selector: 'app-customers',
+    templateUrl: './customers.component.html',
+    styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
 
-  displayedColumns: string[] = ['name', 'companyIdentificationNumber', 'taxIdentificationNumber', 'town', 'actions'];
-  dataSource = new MatTableDataSource<Customer>(ELEMENT_DATA);
+    displayedColumns: string[] = ['name', 'companyIdentificationNumber', 'taxIdentificationNumber', 'town', 'actions'];
+    dataSource = new MatTableDataSource<Customer>(ELEMENT_DATA);
 
-  customers: any = [];
+    customers: any = [];
+    sure: string;
 
-  constructor(private router: Router, private rest: RestService) {
-  }
-
-  ngOnInit() {
-    this.getCustomers();
-  }
-
-  onEdit(customer) {
-    this.selectRow(customer);
-  }
-
-  onDelete(customer) {
-    this.removeCustomer(customer.uuid);
-  }
-
-  selectRow(customer) {
-    this.router.navigate(['/customers/' + customer.uuid]);
-  }
-
-  newEntry() {
-    this.router.navigate(['/customers/new-customer']);
-  }
-
-  getCustomers() {
-    this.customers = [];
-    this.rest.getCustomers().subscribe((data: {}) => {
-      // @ts-ignore
-      this.dataSource = new MatTableDataSource<Customer>(data);
-    });
-  }
-
-  removeCustomer(uuid) {
-    if (confirm('Ste si istý?')) {
-      this.rest.removeCustomer(uuid).subscribe((data: {}) => {
-        this.getCustomers();
-      });
+    constructor(private router: Router, private rest: RestService, private translate: TranslateService) {
+        translate.setDefaultLang(LangService.getLanguage());
+        translate.get('customers.sure').subscribe((translated: string) => {
+            this.sure = translated;
+        });
     }
-  }
+
+    ngOnInit() {
+        this.getCustomers();
+    }
+
+    onEdit(customer) {
+        this.selectRow(customer);
+    }
+
+    onDelete(customer) {
+        this.removeCustomer(customer.uuid);
+    }
+
+    selectRow(customer) {
+        this.router.navigate(['/customers/' + customer.uuid]);
+    }
+
+    newEntry() {
+        this.router.navigate(['/customers/new-customer']);
+    }
+
+    getCustomers() {
+        this.customers = [];
+        this.rest.getCustomers().subscribe((data: {}) => {
+            // @ts-ignore
+            this.dataSource = new MatTableDataSource<Customer>(data);
+        });
+    }
+
+    removeCustomer(uuid) {
+        if (confirm(this.sure)) {
+            this.rest.removeCustomer(uuid).subscribe((data: {}) => {
+                this.getCustomers();
+            });
+        }
+    }
 }
